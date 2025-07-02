dee9d56684493ccdbcc8337eb76234674f58adc9
Diyorbek Turg'unboyev <turgunboyev504@gmail.com>
2025-07-03 01:45:47 +0500

    Initial commit

---
+++ src/Controllers/MirpayController.php
@@ -0,0 +1,80 @@
+<?php
+namespace TurgunboyevUz\Mirpay\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use TurgunboyevUz\Mirpay\Enums\MirpayState;
+use TurgunboyevUz\Mirpay\Exceptions\MirpayException;
+use TurgunboyevUz\Mirpay\Models\MirpayTransaction;
+
+class MirpayController extends Controller
+{
+    public function success(Request $request)
+    {
+        $request->validate([
+            'payid' => 'required|string|exists:mirpay_transactions,transaction_id',
+            'summa' => 'required|numeric',
+        ]);
+
+        $transaction = MirpayTransaction::where('transaction_id', $request->payid)->first();
+
+        if (! $transaction) {
+            throw new MirpayException('Transaction not found', Response::HTTP_NOT_FOUND);
+        }
+
+        if ($transaction->state !== MirpayState::PENDING) {
+            throw new MirpayException('Transaction already has been accepted', Response::HTTP_CONFLICT);
+        }
+
+        if ($request->summa != $transaction->amount) {
+            throw new MirpayException('Transaction amounts do not match', Response::HTTP_CONFLICT);
+        }
+
+        $transaction->state = 1;
+        $transaction->save();
+
+        $model = $transaction->payable;
+
+        require_once app_path('Payment/after_success.php');
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaction accepted',
+        ]);
+    }
+
+    public function fail(Request $request)
+    {
+        $request->validate([
+            'payid' => 'required|string|exists:mirpay_transactions,transaction_id',
+            'summa' => 'required|numeric',
+        ]);
+
+        $transaction = MirpayTransaction::where('transaction_id', $request->payid)->first();
+
+        if (! $transaction) {
+            throw new MirpayException('Transaction not found', Response::HTTP_NOT_FOUND);
+        }
+
+        if ($transaction->state !== MirpayState::PENDING) {
+            throw new MirpayException('Transaction already has been accepted', Response::HTTP_CONFLICT);
+        }
+
+        if ($request->summa != $transaction->amount) {
+            throw new MirpayException('Transaction amounts do not match', Response::HTTP_CONFLICT);
+        }
+
+        $transaction->state = 0;
+        $transaction->save();
+
+        $model = $transaction->payable;
+
+        require_once app_path('Payment/after_fail.php');
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaction accepted',
+        ]);
+    }
+}
